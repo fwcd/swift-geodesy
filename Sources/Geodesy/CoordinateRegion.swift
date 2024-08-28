54e28b13d422d9ec736e3bdd358fecc846a09bdd
fwcd <fwcd@fwcd.dev>
2024-08-29 00:47:26 +0200

    Add CoordinateSpan and reimplement CoordinateRegion

--- Sources/Geodesy/CoordinateRegion.swift
+++ Sources/Geodesy/CoordinateRegion.swift
@@ -1,26 +1,42 @@
-public struct CoordinateRegion: Codable, Hashable {
-    public let topLeft: Coordinates
-    public let bottomRight: Coordinates
+/// A positioned rectangular geographical region.
+public struct CoordinateRegion: Hashable, Codable {
+    /// The coordinates of the region's center.
+    public let center: Coordinates
+    /// The size of the region.
+    public let span: CoordinateSpan
+
+    /// The top left corner of the region.
+    public var topLeft: Coordinates {
+        Coordinates(
+            latitude: center.latitude - span.latitudeDelta / 2,
+            longitude: center.longitude - span.longitudeDelta / 2
+        )
+    }
+
+    /// The bottom right corner of the region.
+    public var bottomRight: Coordinates {
+        Coordinates(
+            latitude: center.latitude + span.latitudeDelta / 2,
+            longitude: center.longitude + span.longitudeDelta / 2
+        )
+    }
     
+    /// The top right corner of the region.
     public var topRight: Coordinates {
         Coordinates(
             latitude: topLeft.latitude,
             longitude: bottomRight.longitude
         )
     }
+
+    /// The bottom left corner of the region.
     public var bottomLeft: Coordinates {
         Coordinates(
             latitude: bottomRight.latitude,
             longitude: topLeft.longitude
         )
     }
-    public var center: Coordinates {
-        Coordinates(
-            latitude: (topLeft.latitude + bottomRight.latitude) / 2,
-            longitude: (topLeft.longitude + bottomRight.longitude) / 2
-        )
-    }
-    
+
     public func contains(_ location: Coordinates) -> Bool {
         // TODO: This naive version may fail where coordinates wrap around
         (min(topLeft.latitude, bottomRight.latitude)...max(topLeft.latitude, bottomRight.latitude)).contains(location.latitude)
---
+++ Sources/Geodesy/CoordinateSpan.swift
@@ -0,0 +1,56 @@
+/// An unpositioned rectangular geographical size.
+public struct CoordinateSpan: Hashable, Codable, AdditiveArithmetic {
+    public static let zero = Self(latitudeDelta: .zero, longitudeDelta: .zero)
+
+    /// The height of the region in degrees.
+    public let latitudeDelta: Degrees
+    /// The width of the region in degrees.
+    public var longitudeDelta: Degrees
+
+    public init(latitudeDelta: Degrees, longitudeDelta: Degrees) {
+        self.latitudeDelta = latitudeDelta
+        self.longitudeDelta = longitudeDelta
+    }
+
+    public func transposed() -> Self {
+        Self(
+            latitudeDelta: longitudeDelta,
+            longitudeDelta: latitudeDelta
+        )
+    }
+
+    public static func +(lhs: Self, rhs: Self) -> Self {
+        Self(
+            latitudeDelta: lhs.latitudeDelta + rhs.latitudeDelta,
+            longitudeDelta: lhs.longitudeDelta + rhs.longitudeDelta
+        )
+    }
+
+    public static func -(lhs: Self, rhs: Self) -> Self {
+        Self(
+            latitudeDelta: lhs.latitudeDelta - rhs.latitudeDelta,
+            longitudeDelta: lhs.longitudeDelta - rhs.longitudeDelta
+        )
+    }
+
+    public static func *(lhs: Self, rhs: Double) -> Self {
+        Self(
+            latitudeDelta: lhs.latitudeDelta * rhs,
+            longitudeDelta: lhs.longitudeDelta * rhs
+        )
+    }
+
+    public static func *(lhs: Double, rhs: Self) -> Self {
+        Self(
+            latitudeDelta: lhs * rhs.latitudeDelta,
+            longitudeDelta: lhs * rhs.longitudeDelta
+        )
+    }
+
+    public static func /(lhs: Self, rhs: Double) -> Self {
+        Self(
+            latitudeDelta: lhs.latitudeDelta / rhs,
+            longitudeDelta: lhs.longitudeDelta / rhs
+        )
+    }
+}

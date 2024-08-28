605b1d0469c6a11dd007655f5c054d2ec6a91859
fwcd <fwcd@fwcd.dev>
2024-08-29 00:40:10 +0200

    Add DocC plugin

--- Package.swift
+++ Package.swift
@@ -12,6 +12,9 @@ let package = Package(
             targets: ["Geodesy"]
         ),
     ],
+    dependencies: [
+                .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.1.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.

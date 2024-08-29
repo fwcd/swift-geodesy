05cb2b99af4e36b27da6257ff91802367261bb78
fwcd <fwcd@fwcd.dev>
2024-08-29 02:30:08 +0200

    Make SignedCardinal protocol public

--- Sources/Geodesy/SignedCardinal.swift
+++ Sources/Geodesy/SignedCardinal.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-protocol SignedCardinal {
+public protocol SignedCardinal {
     var sign: FloatingPointSign { get }
     
     init(sign: FloatingPointSign)
 }
 
-extension SignedCardinal {
+public extension SignedCardinal {
     static var plus: Self { Self(sign: .plus) }
     static var minus: Self { Self(sign: .minus) }
 }

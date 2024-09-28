eed981625480bfb95406eeed278c1660ddc55d76
fwcd <fwcd@fwcd.dev>
2024-09-28 02:30:06 +0200

    Add more test cases to CoordinateRegion

--- Tests/GeodesyTests/CoordinateRegionTests.swift
+++ Tests/GeodesyTests/CoordinateRegionTests.swift
@@ -14,4 +14,28 @@ class CoordinateRegionTests: XCTestCase {
         assertApproxEquals(region.minCorner, region2.minCorner)
         assertApproxEquals(region.maxCorner, region2.maxCorner)
     }
+
+    func testEdges() {
+        let bottomLeft = Coordinates(latitude: 10, longitude: 20)
+        let topRight = Coordinates(latitude: 30, longitude: 40)
+        let region = CoordinateRegion(bottomLeft: bottomLeft, topRight: topRight)
+
+        assertApproxEquals(region.topLeft.latitude, region.topRight.latitude)
+        assertApproxEquals(region.bottomLeft.latitude, region.bottomRight.latitude)
+
+        assertApproxEquals(region.bottomLeft.longitude, region.topLeft.longitude)
+        assertApproxEquals(region.bottomRight.longitude, region.topRight.longitude)
+    }
+
+    func testResizing() {
+        let bottomLeft = Coordinates(latitude: 10, longitude: 20)
+        let topRight = Coordinates(latitude: 30, longitude: 50)
+        var region = CoordinateRegion(bottomLeft: bottomLeft, topRight: topRight)
+
+        region.bottomLeft.latitude.totalDegrees = 15
+        assertApproxEquals(region.bottomLeft, .init(latitude: 15, longitude: 20))
+        assertApproxEquals(region.bottomRight, .init(latitude: 15, longitude: 50))
+        assertApproxEquals(region.topLeft, .init(latitude: 30, longitude: 20))
+        assertApproxEquals(region.topRight, .init(latitude: 30, longitude: 50))
+    }
 }
